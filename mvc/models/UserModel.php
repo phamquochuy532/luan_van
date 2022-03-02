d30ab52c47042243f4eabe59edd69a574cc8743e
unknown <huy3180712@gmail.com>
2022-03-02 15:11:19 +0700

    add confirm firsttime

--- mvc/models/UserModel.php
+++ mvc/models/UserModel.php
@@ -1,4 +1,11 @@
 <?php
+
+include_once(APP_ROOT . '/libs/PHPMailer.php');
+include_once(APP_ROOT . '/libs/Exception.php');
+include_once(APP_ROOT . '/libs/SMTP.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+
 class UserModel extends DB
 {
     public function checkLogin($email, $password)
@@ -14,8 +21,38 @@ class UserModel extends DB
     }
     public function insert($fullName,$email, $dob, $address, $password)
     {
-        $sql = "INSERT INTO Users(`id`, `fullName`, `email`, `dob`, `address`, `password`, `roleId`, `isConfirmed`) VALUES (NULL,'$fullName','$email','$dob','$address','$password',1,1)";
+        //generate captcha
+
+        $captcha = rand(100000, 999999);
+        $sql = "INSERT INTO Users(`id`, `fullName`, `email`, `dob`, `address`, `password`, `roleId`, `status`, `captcha`, `isConfirmed`) 
+        VALUES (NULL,'$fullName','$email','$dob','$address','$password',1,1, '$captcha', 0)";
         $result = mysqli_query($this->con, $sql);
-        return $result;
+        if ($result) {
+            // send email
+            $mail = new PHPMailer();
+            $mail->Mailer = "smtp";
+
+            $mail->SMTPDebug  = 0;
+            $mail->SMTPAuth   = TRUE;
+            $mail->SMTPSecure = "tls";
+            $mail->Port       = 587;
+            $mail->Host       = "smtp.gmail.com";
+            $mail->Username   = "huy3180712@gmail.com";
+            $mail->Password   = "181201Huy";
+
+            $mail->IsHTML(true);
+            $mail->CharSet = 'UTF-8';
+            $mail->AddAddress($email, "recipient-name");
+            $mail->SetFrom("huy3180712@gmail.com", "HUYPHAM STORE");
+            $mail->Subject = "Xác nhận email tài khoản - HUYPHAM STORE";
+            $mail->Body = "<h3>Cảm ơn bạn đã đăng ký tài khoản tại website HUYPHAM STORE</h3></br>Đây là mã xác minh tài khoản của bạn: " . $captcha . "";
+
+            $mail->Send();
+            var_dump($mail);
+            die();
+            return true;
+        }else {
+            return false;
+        }
     }
 }
--- mvc/views/register.php
+++ mvc/views/register.php
@@ -13,10 +13,20 @@
             <p><input type="email" placeholder="Email" name="email" required></p>
             <p><input type="date" name="dob" required></p>
             <p><input type="text" placeholder="Địa chỉ" name="address" required></p>
-            <p><input type="password" placeholder="Mật khẩu" name="password" required></p>
+            <p><input type="password" id="password" placeholder="Mật khẩu" name="password" required></p>
+            <p><input type="password" placeholder="Nhập lại mật khẩu" name="repassword" required oninput="check(this)"></p>
             <p class="<?= $data["cssClass"] ?>"><?= isset($data['message']) ? $data['message'] : "" ?></p>
             <p><input type="submit" value="Đăng ký"></p>
         </form>
     </div>
     <?php require APP_ROOT . '/views/inc/footer.php';?>
+    <script>
+        function check(input){
+            if (input.value != document.getElementById('password').value){
+                input.setCustomValidity('Nhập lại mật khaaue không trùng khớp');
+            }else {
+                input.setCustomValidity('');
+            }
+        }
+    </script>
 </body>
